89b4f6936ba341339ef59e2f9b2b089fbbd9af43
MARSH420 <evilmarsh420@gmail.com>
2026-03-11 11:47:45 +0200

    Update config_flow.py

--- custom_components/phd_solutions/config_flow.py
+++ custom_components/phd_solutions/config_flow.py
@@ -1,17 +1,34 @@
 import voluptuous as vol
+
 from homeassistant import config_entries
-from .const import DOMAIN, DEFAULT_HOST, DEFAULT_PORT
+from homeassistant.const import CONF_HOST, CONF_PORT
+
+from .const import (
+    DOMAIN,
+    DEFAULT_HOST,
+    DEFAULT_PORT,
+    DEFAULT_INPUTS,
+    DEFAULT_OUTPUTS,
+    CONF_INPUTS,
+    CONF_OUTPUTS,
+    CONF_MODEL,
+)
+
 from .controller import PHDController
 
+
 class PHDConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for PHD Solutions."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
-            host = user_input.get("host", DEFAULT_HOST)
-            port = user_input.get("port", DEFAULT_PORT)
+            host = user_input.get(CONF_HOST, DEFAULT_HOST)
+            port = user_input.get(CONF_PORT, DEFAULT_PORT)
+
             controller = PHDController(host, port)
 
             try:
@@ -19,22 +36,37 @@ class PHDConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "cannot_connect"
             else:
+                inputs = user_input.get(CONF_INPUTS, DEFAULT_INPUTS)
+                outputs = user_input.get(CONF_OUTPUTS, DEFAULT_OUTPUTS)
+
+                model = f"{inputs}x{outputs}"
+
                 return self.async_create_entry(
-                    title=f"PHD 4K444 ({host})",
-                    data=user_input,
+                    title=f"PHD Solutions ({host})",
+                    data={
+                        CONF_HOST: host,
+                        CONF_PORT: port,
+                        CONF_INPUTS: inputs,
+                        CONF_OUTPUTS: outputs,
+                        CONF_MODEL: model,
+                    },
                 )
 
-        data_schema = vol.Schema({
-            vol.Required('host', default=DEFAULT_HOST): str,
-            vol.Required('port', default=DEFAULT_PORT): int,
-        })
-        return self.async_show_form(step_id='user', data_schema=data_schema, errors=errors)
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_HOST, default=DEFAULT_HOST): str,
+                vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+                vol.Required(CONF_INPUTS, default=DEFAULT_INPUTS): vol.All(
+                    int, vol.Range(min=1, max=16)
+                ),
+                vol.Required(CONF_OUTPUTS, default=DEFAULT_OUTPUTS): vol.All(
+                    int, vol.Range(min=1, max=16)
+                ),
+            }
+        )
 
-    async def async_step_options(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title='', data=user_input)
-
-        data_schema = vol.Schema({
-            vol.Required('debug', default=False): bool,
-        })
-        return self.async_show_form(step_id='options', data_schema=data_schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+            errors=errors,
+        )
